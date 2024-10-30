b2eef0d2f70e6599410acc4b05a0736b1d52576c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-10-30 16:02:40 +0700

    add scrollbar

--- lib/src/widgets/recipe_slider_widget.dart
+++ lib/src/widgets/recipe_slider_widget.dart
@@ -2,21 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:recipe_app_flutter/src/model/recipe_model.dart';
 import 'package:recipe_app_flutter/src/screens/detail_screen.dart';
 
-class RecipeSliderWidget extends StatelessWidget {
+class RecipeSliderWidget extends StatefulWidget {
   final String selectedCategory;
   final String searchInput;
   const RecipeSliderWidget(
       {super.key, required this.selectedCategory, required this.searchInput});
 
+  @override
+  State<RecipeSliderWidget> createState() => _RecipeSliderWidgetState();
+}
+
+class _RecipeSliderWidgetState extends State<RecipeSliderWidget> {
+  final _scrollController = ScrollController();
+
   List<RecipeData> getRecipe() {
     return recipeDataList.where((recipe) {
       // For "All" category, don't filter by category
-      bool categoryMatch = selectedCategory == 'All' ||
-          recipe.category.toLowerCase() == selectedCategory.toLowerCase();
+      bool categoryMatch = widget.selectedCategory == 'All' ||
+          recipe.category.toLowerCase() ==
+              widget.selectedCategory.toLowerCase();
 
       // If search is empty, don't filter by search
-      bool searchMatch = searchInput.isEmpty ||
-          recipe.name.toLowerCase().contains(searchInput.toLowerCase());
+      bool searchMatch = widget.searchInput.isEmpty ||
+          recipe.name.toLowerCase().contains(widget.searchInput.toLowerCase());
 
       return categoryMatch && searchMatch;
     }).toList();
@@ -25,92 +33,78 @@ class RecipeSliderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<RecipeData> filteredRecipes = getRecipe();
-
     return SizedBox(
         height: 500,
         child: filteredRecipes.isNotEmpty
-            ? ListView(
-                scrollDirection: Axis.horizontal,
-                children: filteredRecipes.map((RecipeData recipe) {
-                  return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (BuildContext context) => DetailScreen(
-                                      recipe: recipe,
-                                    )));
-                      },
-                      child: Container(
-                          height: 500,
-                          width: 300,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20)),
-                          padding: const EdgeInsets.all(10),
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Stack(
-                                clipBehavior: Clip.hardEdge,
-                                alignment: AlignmentDirectional.bottomStart,
-                                children: [
-                                  Image.network(
-                                    recipe.imageUrl,
-                                    fit: BoxFit.cover,
-                                    height: double.infinity,
-                                  ),
-                                  Container(
-                                    padding: const EdgeInsets.all(5.0),
-                                    alignment: Alignment.bottomLeft,
-                                    decoration: BoxDecoration(
-                                      gradient: LinearGradient(
-                                        begin: Alignment.topCenter,
-                                        end: Alignment.bottomCenter,
-                                        colors: <Color>[
-                                          Colors.black.withAlpha(0),
-                                          Colors.black26,
-                                          Colors.black87
-                                        ],
-                                      ),
+            ? Scrollbar(
+                controller: _scrollController,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _scrollController,
+                  children: filteredRecipes.map((RecipeData recipe) {
+                    return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      DetailScreen(
+                                        recipe: recipe,
+                                      )));
+                        },
+                        child: Container(
+                            height: 500,
+                            width: 300,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20)),
+                            padding: const EdgeInsets.all(10),
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Stack(
+                                  clipBehavior: Clip.hardEdge,
+                                  alignment: AlignmentDirectional.bottomStart,
+                                  children: [
+                                    Image.network(
+                                      recipe.imageUrl,
+                                      fit: BoxFit.cover,
+                                      height: double.infinity,
                                     ),
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(10),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.stretch,
-                                          mainAxisSize: MainAxisSize.max,
-                                          children: [
-                                            Text(
-                                              recipe.name,
-                                              style: TextStyle(
-                                                  fontSize: 18,
-                                                  fontWeight: FontWeight.bold,
-                                                  color: Colors.white),
-                                            ),
-                                            SizedBox(
-                                              height: 10,
-                                            ),
-                                            Row(
-                                              children: [
-                                                Container(
-                                                  padding:
-                                                      const EdgeInsets.all(8),
-                                                  decoration: BoxDecoration(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            20),
-                                                    color: Colors.white12,
-                                                  ),
-                                                  child: Text(
-                                                    recipe.category,
-                                                    style: TextStyle(
-                                                        color: Colors.white),
-                                                  ),
-                                                ),
-                                                SizedBox(width: 10),
-                                                Flexible(
-                                                  child: Container(
+                                    Container(
+                                      padding: const EdgeInsets.all(5.0),
+                                      alignment: Alignment.bottomLeft,
+                                      decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                          begin: Alignment.topCenter,
+                                          end: Alignment.bottomCenter,
+                                          colors: <Color>[
+                                            Colors.black.withAlpha(0),
+                                            Colors.black26,
+                                            Colors.black87
+                                          ],
+                                        ),
+                                      ),
+                                      child: Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.stretch,
+                                            mainAxisSize: MainAxisSize.max,
+                                            children: [
+                                              Text(
+                                                recipe.name,
+                                                style: TextStyle(
+                                                    fontSize: 18,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.white),
+                                              ),
+                                              SizedBox(
+                                                height: 10,
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Container(
                                                     padding:
                                                         const EdgeInsets.all(8),
                                                     decoration: BoxDecoration(
@@ -120,23 +114,42 @@ class RecipeSliderWidget extends StatelessWidget {
                                                       color: Colors.white12,
                                                     ),
                                                     child: Text(
-                                                      recipe.tags,
-                                                      overflow:
-                                                          TextOverflow.ellipsis,
+                                                      recipe.category,
                                                       style: TextStyle(
                                                           color: Colors.white),
                                                     ),
                                                   ),
-                                                )
-                                              ],
-                                            )
-                                          ],
-                                        )),
-                                  ),
-                                ],
-                              ))));
-                }).toList(),
-              )
+                                                  SizedBox(width: 10),
+                                                  Flexible(
+                                                    child: Container(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              8),
+                                                      decoration: BoxDecoration(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(20),
+                                                        color: Colors.white12,
+                                                      ),
+                                                      child: Text(
+                                                        recipe.tags,
+                                                        overflow: TextOverflow
+                                                            .ellipsis,
+                                                        style: TextStyle(
+                                                            color:
+                                                                Colors.white),
+                                                      ),
+                                                    ),
+                                                  )
+                                                ],
+                                              )
+                                            ],
+                                          )),
+                                    ),
+                                  ],
+                                ))));
+                  }).toList(),
+                ))
             : SizedBox(
                 height: 500,
                 child: Center(
@@ -146,5 +159,11 @@ class RecipeSliderWidget extends StatelessWidget {
                   ),
                 ),
               ));
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
